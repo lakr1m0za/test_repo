004bf2eaf99dcba0cbc568d2027c285cbabe552a
lakr1m0za <mohax8romeo@gmail.com>
2019-08-20 00:10:26 +0300

    Add test template

--- test.py
+++ test.py
@@ -1,2 +1,11 @@
-a = 'Anna'
-print ("Hello, ",a)
+import random
+s = '{} is {} in Spain'
+L = ['Cero', 'Uno', 'Dos', 'Tres', 'Cuatro', 'Cinco', 'Seis', 'Siete', 'Ocho', 'Nueve']
+#1
+n = random.randint(0, 9)
+#2
+x = L[n]
+#3
+print (x)
+s = s.format(x, n)
+#4
