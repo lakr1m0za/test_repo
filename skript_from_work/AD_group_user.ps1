dcf42c86e10196bfa886f83f075f50be8610d97d
Artyom <52546816+lakr1m0za@users.noreply.github.com>
2019-12-10 13:08:00 +0200

    Update AD_group_user.ps1

--- skript_from_work/AD_group_user.ps1
+++ skript_from_work/AD_group_user.ps1
@@ -1,9 +1,9 @@
 ﻿Import-Module activedirectory
-get-adgroupmember "UAKHAA05RDP" | select name | Export-csv -path c:\temp\UAKHAA05RDP-2019.csv -Append -Encoding UTF8 -NoTypeInformation
-get-adgroupmember "UAKHAA17RDP" | select name | Export-csv -path c:\temp\UAKHAA17RDP-2019.csv -Append -Encoding UTF8 -NoTypeInformation
-get-adgroupmember "UAKHAT01RDP" | select Name | Export-csv -path c:\temp\UAKHAT01RDP-2019.csv -Append -Encoding UTF8 -NoTypeInformation
-get-adgroupmember "UAKHAT02RDP" | select Name | Export-csv -path c:\temp\UAKHAT02RDP-2019.csv -Append -Encoding UTF8 -NoTypeInformation
-get-adgroupmember "UAKHAD98RDP" | select Name | Export-csv -path c:\temp\UAKHAD98RDP-2019.csv -Append -Encoding UTF8 -NoTypeInformation
-get-adgroupmember "UAKHAD99RDP" | select Name | Export-csv -path c:\temp\UAKHAD99RDP-2019.csv -Append -Encoding UTF8 -NoTypeInformation
-get-adgroupmember "UAKHAT01RDPSession" | select Name | Export-csv -path c:\temp\UAKHAT01RDPSession-2019.csv -Append -Encoding UTF8 -NoTypeInformation
-get-adgroupmember "UAKHAT02RDPSe" | select Name | Export-csv -path c:\temp\UAKHAT02RDPSe-2019.csv -Append -Encoding UTF8 -NoTypeInformation
+get-adgroupmember "name1" | select name | Export-csv -path c:\temp\name1-2019.csv -Append -Encoding UTF8 -NoTypeInformation
+get-adgroupmember "name2" | select name | Export-csv -path c:\temp\name2-2019.csv -Append -Encoding UTF8 -NoTypeInformation
+get-adgroupmember "name3" | select Name | Export-csv -path c:\temp\name3-2019.csv -Append -Encoding UTF8 -NoTypeInformation
+get-adgroupmember "name4" | select Name | Export-csv -path c:\temp\name4-2019.csv -Append -Encoding UTF8 -NoTypeInformation
+get-adgroupmember "name5" | select Name | Export-csv -path c:\temp\name5-2019.csv -Append -Encoding UTF8 -NoTypeInformation
+get-adgroupmember "name6" | select Name | Export-csv -path c:\temp\name6-2019.csv -Append -Encoding UTF8 -NoTypeInformation
+get-adgroupmember "name7" | select Name | Export-csv -path c:\temp\name7-2019.csv -Append -Encoding UTF8 -NoTypeInformation
+get-adgroupmember "name8" | select Name | Export-csv -path c:\temp\name8-2019.csv -Append -Encoding UTF8 -NoTypeInformation
